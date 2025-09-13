ef7f2f4f83f02d0ed649e14dc896072e5a87ec3d
MarshalSud <ss156831@gmail.com>
2025-09-13 18:10:28 +0530

    add files name.py and if-else.py to solve the fix

---
+++ if-else.py
@@ -0,0 +1,5 @@
+a = 10 
+if a <= 10:
+    print ('no is less than 10 or can be 10')
+else:
+    print('no is sure shot more than 10')
---
+++ name.py
@@ -0,0 +1,5 @@
+number1= 10 
+number2= 20 
+addition = 10 + 20 
+print(addition)
+print('sudarshan')
