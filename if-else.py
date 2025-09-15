863511cf769a2e004718f86e8a94891cf26864a8
MarshalSud <ss156831@gmail.com>
2025-09-15 16:04:15 +0530

    add these files in the branch 5th branch

--- if-else.py
+++ if-else.py
@@ -1,5 +1,6 @@
 a = 10 
 if a <= 10:
     print ('no is less than 10 or can be 10')
+    print ('the no is not clear, can you please tell me the real no')
 else:
     print('no is sure shot more than 10')
