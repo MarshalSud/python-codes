faefda4ed430a659ed825b1549556de6809b6475
MarshalSud <ss156831@gmail.com>
2025-09-21 14:55:49 +0530

    update name.py file

--- name.py
+++ name.py
@@ -3,4 +3,5 @@ number2= 20
 addition = 10 + 20 
 print(addition)
 print('sudarshan is the best east or the west he is the best')
+print("sometimes i feel can i really reach to that goal, can i really do it?")
 print ('meghana')
